0c8ed878e469b05179a36bb6e9442c457ddd3534
Jennifer Tongco <136764299+JenniferTongco@users.noreply.github.com>
2025-02-17 14:28:26 +0800

    Add numMessages flag and fix endpoint selection
    to improve efficiency during testing

--- message_publisher/message_publisher.go
+++ message_publisher/message_publisher.go
@@ -8,6 +8,7 @@ import (
 	"net/http"
 	"sync"
 	"time"
+	"flag"
 )
 
 type Message struct {
@@ -16,22 +17,41 @@ type Message struct {
     Payload      string `json:"payload"`
 }
 
-const (
-	numMessages = 10000
+
+var (
+	numMessages=flag.Int("numMessages", 10000, "Number of messages to publish")
+	useMock     = flag.Bool("mock", false, "Use localhost mock servers (ports 8080, 8081, 8082)")
+	useBulkWrite = flag.Bool("bulkwrite", false, "Use bulk write GCP endpoints (ports 8080)")
 )
 
-var endpoints = []string{ 
-"http://10.146.0.4:8080/write",
-"http://10.146.0.8:8080/write",
-"http://10.146.0.18:8080/write",
-}
+
+var (
+	directWriteEndpoints = []string{ 
+		"http://10.146.0.4:8085/write",
+		"http://10.146.0.8:8085/write",
+		"http://10.146.0.18:8085/write",
+	}
+	
+	 bulkWriteEndpoints = []string{
+		"http://10.146.0.4:8080/write",
+		"http://10.146.0.8:8080/write",
+		"http://10.146.0.18:8080/write",
+	}
+	
+	 mockEndpoints = []string{
+		"http://localhost:8080/write",
+		"http://localhost:8081/write",
+		"http://localhost:8082/write",
+	}
+	activeEndpoints []string
+)
 
 func publishMessage(wg *sync.WaitGroup, id int, endpoint string) {
 	defer wg.Done()
 	msg := Message{
 		ID:           fmt.Sprintf("%d", id),
 		Subsription:  fmt.Sprintf("sub-%d", id),
-		Payload:      fmt.Sprintf("MESSAGE TO NODE %d", id%len(endpoints)+1),
+		Payload:      fmt.Sprintf("MESSAGE TO NODE %d", id%len(activeEndpoints)+1),
 	}
 
 	data, err := json.Marshal(msg)
@@ -49,13 +69,28 @@ func publishMessage(wg *sync.WaitGroup, id int, endpoint string) {
 	log.Printf("%s Message %d (ID: %s) published successfully", time.Now().Format(time.RFC3339), id, msg.ID)
 }
 
+
 func main() {
+	flag.Parse()
+
+	if *useMock {
+		activeEndpoints = mockEndpoints
+		log.Println("Using mock servers at localhost:8080, localhost:8081, localhost:8082")
+	} else if *useBulkWrite {
+		activeEndpoints = bulkWriteEndpoints
+		log.Println("Using bulk write GCP endpoints (ports 8080)")
+	} else {
+		activeEndpoints = directWriteEndpoints
+		log.Println("Using direct write GCP endpoints (ports 8080)")
+	}
+
+	
 	var wg sync.WaitGroup
 	startTime := time.Now()
 
 	for i := 0; i < numMessages; i++ {
 		wg.Add(1)
-		endpoint := endpoints[i%len(endpoints)] // Round-robin selection
+		endpoint := endpoints[i%len(activeEndpoints)] // Round-robin distribution
 		go publishMessage(&wg, i, endpoint)
 	}
 
