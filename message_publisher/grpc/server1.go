e8e568934125bc0be2b0ad0a7d8a062ad3067d67
Jennifer Tongco <jennifer.j.tongco@gmail.com>
2025-02-18 10:46:22 +0800

    added mock server for local testing (uses gRPC)

---
+++ message_publisher/grpc/server1.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"context"
+	"log"
+	"net"
+
+	pb "github.com/alphauslabs/pubsub-proto"
+	"google.golang.org/grpc"
+)
+
+type server struct {
+	pb.UnimplementedPubSubServiceServer
+}
+
+func (s *server) Publish(ctx context.Context, msg *pb.Message) (*pb.PublishResponse, error) {
+	log.Printf("[Mock Server 8081] Received message: ID=%s, Subscription=%s, Payload=%s",
+		msg.Id, msg.Subsription, string(msg.Payload))
+	return &pb.PublishResponse{MessageId: msg.Id}, nil
+}
+
+func main() {
+	listener, err := net.Listen("tcp", ":8081")
+	if err != nil {
+		log.Fatalf("[FATAL] Failed to listen on port 8081: %v", err)
+	}
+
+	grpcServer := grpc.NewServer()
+	pb.RegisterPubSubServiceServer(grpcServer, &server{})
+
+	log.Println("[INFO] Mock gRPC Server is running on port 8081")
+	if err := grpcServer.Serve(listener); err != nil {
+		log.Fatalf("[FATAL] Failed to serve gRPC server: %v", err)
+	}
+}
