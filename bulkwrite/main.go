1567f51abe999bd227cf18eec20f0909eeaf6d6e
Scansky <43263908+cnhy-nero-diskard@users.noreply.github.com>
2025-02-19 16:16:58 +0800

    rev

--- bulkwrite/main.go
+++ bulkwrite/main.go
@@ -24,7 +24,7 @@ import (
 var (
 	isLeader       = flag.Bool("leader", false, "Run this node as the leader for bulk writes")
 	leaderURL      = flag.String("leader-url", "http://35.243.83.115:50050", "URL of the leader node")
-	followerPort   = flag.String("follower-port", "50050", "Port for the follower node") // New flag for follower port
+	followerPort   = flag.String("follower-port", "50051", "Port for the follower node") // New flag for follower port
 	batchSize      = flag.Int("batchsize", 5000, "Batch size for bulk writes")
 	messagesBuffer = flag.Int("messagesbuffer", 1000000, "Buffer size for messages channel")
 	waitTime       = flag.Duration("waittime", 500*time.Millisecond, "Wait time before flushing the batch")
