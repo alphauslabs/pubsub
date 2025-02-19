ad44a84b6e3278f63f11d96a0923d4ef48082ad4
Jennifer Tongco <136764299+JenniferTongco@users.noreply.github.com>
2025-02-19 10:57:21 +0800

    Update msg_publisher.go  field name from TopicId to Topic
    Align message structure with proto definition

--- message_publisher/grpc/msg_publisher.go
+++ message_publisher/grpc/msg_publisher.go
@@ -54,7 +54,7 @@ func publishMessage(wg *sync.WaitGroup, id int, client pb.PubSubServiceClient) {
 	msg := &pb.Message{
 		Id:      fmt.Sprintf("%d", id),
 		Payload: []byte(fmt.Sprintf("MESSAGE %d", id)),
-		TopicId: topicID,
+		Topic: topicID,
 	}
 
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
