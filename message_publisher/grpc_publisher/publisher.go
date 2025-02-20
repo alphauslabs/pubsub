26381a6b7df9881a4714a93337f04a89e93d506e
Jansen Relator <jansenprelator223@gmail.com>
2025-02-20 12:00:41 +0800

    Added changes

--- message_publisher/grpc_publisher/publisher.go
+++ message_publisher/grpc_publisher/publisher.go
@@ -10,16 +10,15 @@ import (
 
 	pubsubproto "github.com/alphauslabs/pubsub-proto/v1"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/keepalive"
 )
 
-// Flags for customization
 var (
 	numMessages  = flag.Int("numMessages", 10000, "Number of messages to publish")
 	useMock      = flag.Bool("mock", false, "Use localhost mock gRPC servers")
 	useBulkWrite = flag.Bool("bulkwrite", false, "Use bulk write GCP gRPC endpoints")
 )
 
-// gRPC endpoints for different scenarios
 var (
 	directWriteEndpoints = []string{
 		"10.146.0.43:8085",
@@ -44,26 +43,29 @@ var (
 	connections     []*grpc.ClientConn
 )
 
-func publishMessage(wg *sync.WaitGroup, client pubsubproto.PubSubServiceClient, id int) {
-	defer wg.Done()
+const maxWorkers = 100
 
-	topicID := fmt.Sprintf("topic-%d", id%10000)
+func publishMessage(client pubsubproto.PubSubServiceClient, id int) {
+
+	topicID := fmt.Sprintf("topic-%d", id%1000)
 
 	msg := &pubsubproto.Message{
 		Id:      fmt.Sprintf("%d", id),
 		Topic:   topicID,
-		Payload: []byte(fmt.Sprintf("MESSAGE TO NODE %d", id%len(activeEndpoints)+1)),
+		Payload: []byte(fmt.Sprintf("[MESSAGE TO NODE %d]", id%len(activeEndpoints)+1)),
 	}
 
 	ctx := context.Background()
 
-	res, err := client.Publish(ctx, msg)
-	if err != nil {
+	for retry := 0; retry < 3; retry++ {
+		res, err := client.Publish(ctx, msg)
+		if err == nil {
+			log.Printf("[INFO] Message %d published successfully: ID-%s", id, res.MessageId)
+			return
+		}
 		log.Printf("[ERROR] Message %d failed to publish: %v", id, err)
-		return
+		time.Sleep(200 * time.Millisecond)
 	}
-
-	log.Printf("[SUCCESS] Message %d published successfully. Message ID: %s", id, res.MessageId)
 }
 
 func setActiveEndpoints() {
@@ -81,7 +83,15 @@ func createClients() {
 	connections = make([]*grpc.ClientConn, len(activeEndpoints))
 
 	for i, endpoint := range activeEndpoints {
-		conn, err := grpc.Dial(endpoint, grpc.WithInsecure())
+		conn, err := grpc.Dial(endpoint,
+			grpc.WithInsecure(),
+			grpc.WithBlock(),
+			grpc.WithKeepaliveParams(keepalive.ClientParameters{
+				Time:                10 * time.Second,
+				Timeout:             5 * time.Second,
+				PermitWithoutStream: true,
+			}),
+		)
 		if err != nil {
 			log.Fatalf("Failed to connect to gRPC server %s: %v", endpoint, err)
 		}
@@ -93,23 +103,31 @@ func createClients() {
 
 func main() {
 	flag.Parse()
-
 	setActiveEndpoints()
-
 	createClients()
 
 	log.Printf("Active endpoints: %v", activeEndpoints)
 
-	var wg sync.WaitGroup
+	taskCh := make(chan int, *numMessages)
+	wg := sync.WaitGroup{}
+
+	for i := 0; i < maxWorkers; i++ {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			for id := range taskCh {
+				client := clients[id%len(clients)]
+				publishMessage(client, id)
+			}
+		}()
+	}
+
 	startTime := time.Now()
 
 	for i := 0; i < *numMessages; i++ {
-		wg.Add(1)
-
-		client := clients[i%len(clients)]
-
-		go publishMessage(&wg, client, i)
+		taskCh <- i
 	}
+	close(taskCh)
 
 	wg.Wait()
 
