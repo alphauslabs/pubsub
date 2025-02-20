3619bae84ba9e441cfbbe0f470233f8c545809b5
Jansen Relator <jansenprelator223@gmail.com>
2025-02-20 13:00:29 +0800

    Added changes

--- message_publisher/grpc_publisher/mock_server2.go
+++ message_publisher/grpc_publisher/mock_server2.go
@@ -4,21 +4,42 @@ import (
 	"context"
 	"fmt"
 	"log"
+	"math/rand"
 	"net"
+	"sync"
+
+	"time"
 
 	pubsubproto "github.com/alphauslabs/pubsub-proto/v1"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/keepalive"
 )
 
 type server struct {
 	pubsubproto.UnimplementedPubSubServiceServer
+	messagePool sync.Pool
+}
+
+func newServer() *server {
+	return &server{
+		messagePool: sync.Pool{
+			New: func() interface{} {
+				return fmt.Sprintf("%d", rand.Int63())
+			},
+		},
+	}
 }
 
 func (s *server) Publish(ctx context.Context, msg *pubsubproto.Message) (*pubsubproto.PublishResponse, error) {
-	fmt.Printf("Received message on server 2: %s\n", msg.Payload)
+	messageID := s.messagePool.Get().(string)
+	defer s.messagePool.Put(messageID)
+
+	if rand.Intn(100) < 1 {
+		time.Sleep(time.Millisecond * 10)
+	}
 
 	return &pubsubproto.PublishResponse{
-		MessageId: "02",
+		MessageId: messageID,
 	}, nil
 }
 
@@ -28,15 +49,23 @@ func startMockServer() {
 		log.Fatalf("Failed to listen on port 8081: %v", err)
 	}
 
-	s := grpc.NewServer()
-	pubsubproto.RegisterPubSubServiceServer(s, &server{})
+	grpcServer := grpc.NewServer(
+		grpc.KeepaliveParams(keepalive.ServerParameters{
+			MaxConnectionIdle:     10 * time.Minute,
+			Timeout:               20 * time.Second,
+			MaxConnectionAgeGrace: 5 * time.Minute,
+		}),
+	)
 
-	log.Println("Mock server 2 started on port 8081")
-	if err := s.Serve(lis); err != nil {
+	pubsubproto.RegisterPubSubServiceServer(grpcServer, newServer())
+
+	log.Println("Mock server started on port 8081")
+	if err := grpcServer.Serve(lis); err != nil {
 		log.Fatalf("Failed to serve: %v", err)
 	}
 }
 
 func main() {
+	rand.Seed(time.Now().UnixNano())
 	startMockServer()
 }
