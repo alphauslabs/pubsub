6d07ea45732907b66e1b78d55d4eaf71a9345e30
tituscarl <titusranario@gmail.com>
2025-02-20 20:19:26 +0800

    update publish

--- server.go
+++ server.go
@@ -2,11 +2,14 @@ package main
 
 import (
 	"context"
+	"encoding/json"
 	"log"
 
 	"cloud.google.com/go/spanner"
 	pb "github.com/alphauslabs/pubsub-proto/v1"
 	"github.com/google/uuid"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 )
 
 type server struct {
@@ -19,9 +22,14 @@ const (
 )
 
 func (s *server) Publish(ctx context.Context, in *pb.PublishRequest) (*pb.PublishResponse, error) {
-	log.Println("Received message: ", in)
-	messageID := uuid.New().String()
+	if in.TopicId == "" {
+		return nil, status.Error(codes.InvalidArgument, "topic must not be empty")
+	}
 
+	b, _ := json.Marshal(in)
+	log.Printf("[Publish] Received message:\n%v", string(b))
+
+	messageID := uuid.New().String()
 	mutation := spanner.InsertOrUpdate(
 		MessagesTable,
 		[]string{"id", "topic", "payload", "createdAt", "updatedAt"},
@@ -40,7 +48,6 @@ func (s *server) Publish(ctx context.Context, in *pb.PublishRequest) (*pb.Publis
 		return nil, err
 	}
 
-	log.Printf("Message successfully wrote message to Spanner with ID: %s", messageID)
-
+	log.Printf("[Publish] Message successfully wrote to spanner with ID: %s", messageID)
 	return &pb.PublishResponse{MessageId: messageID}, nil
 }
