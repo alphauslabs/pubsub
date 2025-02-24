b7bb7f1d444ed07271c74c2261ded1bbf13c3d80
Kishea Kate Andoy <142709948+kitkatchoco2002@users.noreply.github.com>
2025-02-24 21:08:39 +0800

    added some sync.map

--- app.go
+++ app.go
@@ -8,4 +8,11 @@ import (
 type PubSub struct {
 	Op     *hedge.Op
 	Client *spanner.Client
+	 // Message handling
+	 messageLocks    sync.Map    // messageID -> MessageLockInfo
+	 messageQueue    sync.Map    // topic -> []*pb.Message
+	 subscriptions  sync.Map    // subscriptionID -> *pb.Subscription
+	 
+	 // Timer tracking
+	 timeoutTimers  sync.Map    // messageID -> *time.Timer
 }
