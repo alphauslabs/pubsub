8fc891b3ea8e5115479701a463b3a5c2e360d539
Jansen Relator <jansenprelator223@gmail.com>
2025-02-24 17:26:17 +0800

    Added changes

--- node_storage/errors.go
+++ node_storage/errors.go
@@ -3,8 +3,9 @@ package storage
 import "errors"
 
 var (
-	ErrMessageNotFound = errors.New("message not found")
-	ErrInvalidMessage  = errors.New("invalid message")
-	ErrTopicNotFound   = errors.New("topic not found")
-	ErrInvalidTopicSub = errors.New("invalid topic-subscription structure")
+	ErrMessageNotFound  = errors.New("message not found")
+	ErrInvalidMessage   = errors.New("invalid message")
+	ErrDuplicateMessage = errors.New("duplicate message")
+	ErrTopicNotFound    = errors.New("topic not found")
+	ErrInvalidTopicSub  = errors.New("invalid topic-subscription structure")
 )
