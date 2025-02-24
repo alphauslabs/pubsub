2cb29fdabb5a4d595bdb54610353d84893f16288
Jansen Relator <jansenprelator223@gmail.com>
2025-02-24 16:07:05 +0800

    Added changes and used the functions in broadcast handlers

--- app/app.go
+++ app/app.go
@@ -2,10 +2,12 @@ package app
 
 import (
 	"cloud.google.com/go/spanner"
+	storage "github.com/alphauslabs/pubsub/node_storage"
 	"github.com/flowerinthenight/hedge/v2"
 )
 
 type PubSub struct {
-	Op     *hedge.Op
-	Client *spanner.Client
+	Op      *hedge.Op
+	Client  *spanner.Client
+	storage *storage.Storage
 }
