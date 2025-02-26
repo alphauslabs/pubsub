7dbbb66cea67d7453f7e4f8f87b1514fb4be3e39
tituscarl <titusranario@gmail.com>
2025-02-26 19:49:13 +0800

    query only new messages for broadcast message

--- broadcast/broadcastmessage.go
+++ broadcast/broadcastmessage.go
@@ -15,7 +15,7 @@ import (
 func FetchAndBroadcastUnprocessedMessage(ctx context.Context, op *hedge.Op, spannerClient *spanner.Client) {
 	ticker := time.NewTicker(20 * time.Second)
 	defer ticker.Stop()
-
+	var lastQueryTime time.Time
 	for {
 		select {
 		case <-ctx.Done():
@@ -23,32 +23,32 @@ func FetchAndBroadcastUnprocessedMessage(ctx context.Context, op *hedge.Op, span
 		case <-ticker.C:
 			l, _ := op.HasLock()
 			if !l {
-				log.Println("Not leader, skipping")
 				continue
 			}
 
+			lastQueryTime = time.Now().UTC()
 			stmt := spanner.Statement{
 				SQL: `SELECT id, topic, payload 
                       FROM Messages
-                      WHERE processed = FALSE`,
+                      WHERE processed = FALSE and createdAt > @lastquerytime`,
+				Params: map[string]interface{}{"lastquerytime": lastQueryTime},
 			}
 
 			iter := spannerClient.Single().Query(ctx, stmt)
 			defer iter.Stop()
-
+			count := 0
 			for {
 				row, err := iter.Next()
 				if err == iterator.Done {
 					break
 				}
 				if err != nil {
-					log.Printf("Error reading message: %v", err)
+					log.Printf("[BroadcastMessage] Error reading message: %v", err)
 					continue
 				}
-
+				count++
 				var msg pb.Message
 				if err := row.Columns(&msg.Id, &msg.Topic, &msg.Payload); err != nil {
-					log.Printf("Error scanning message: %v", err)
 					continue
 				}
 
@@ -66,7 +66,7 @@ func FetchAndBroadcastUnprocessedMessage(ctx context.Context, op *hedge.Op, span
 				// Marshal message info
 				data, err := json.Marshal(messageInfo)
 				if err != nil {
-					log.Printf("Error marshalling message: %v", err)
+					log.Printf("[BroadcastMessage] Error marshalling message: %v", err)
 					continue
 				}
 
@@ -79,24 +79,20 @@ func FetchAndBroadcastUnprocessedMessage(ctx context.Context, op *hedge.Op, span
 				// Marshal broadcast input
 				broadcastData, err := json.Marshal(broadcastInput)
 				if err != nil {
-					log.Printf("Error marshalling broadcast input: %v", err)
+					log.Printf("[BroadcastMessage] Error marshalling broadcast input: %v", err)
 					continue
 				}
 
 				// Broadcast
-				if responses := op.Broadcast(ctx, broadcastData); responses != nil {
-					// get response from each vm
-					for _, response := range responses {
-						log.Printf("[Broadcast] VM %s response for message %s: %v",
-							response.Id,    // vm IP
-							msg.Id,         // Message ID eg. msg1, msg2, etc.
-							response.Error, // <nil> = success, text = error
-						)
+				responses := op.Broadcast(ctx, broadcastData)
+				for _, response := range responses {
+					if response.Error != nil {
+						log.Printf("[BroadcastMessage] Error broadcasting message: %v", response.Error)
 					}
-					continue
 				}
-				//response from all VMs
-				log.Printf("[Broadcast] Error: No responses received for message %s", msg.Id)
+			}
+			if count == 0 {
+				log.Println("[BroadcastMessage] No new unprocessed messages found.")
 			}
 		}
 	}
