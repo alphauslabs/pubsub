5879eb4bb0bc1a93495591bcecaaee65f0d78279
tituscarl <titusranario@gmail.com>
2025-02-27 09:10:49 +0800

    remove todo comment

--- storage/storage.go
+++ storage/storage.go
@@ -133,6 +133,6 @@ func (s *Storage) GetSubscribtionsForTopic(topicID string) ([]string, error) {
 	if !exists {
 		return nil, ErrTopicNotFound
 	}
-	// todo: check if subs is empty meaning no subscription for that topic and also check if a subscription is attaced to a topic
+
 	return subs, nil
 }
