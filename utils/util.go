6439c225e94ef1433922c418ea2b83484fa67953
Horichi <virgilianvidal@gmail.com>
2025-03-05 18:39:19 +0800

    modified logs in utils

--- utils/util.go
+++ utils/util.go
@@ -32,7 +32,7 @@ func EnsureLeaderActive(op *hedge.Op, ctx context.Context) (bool, error) {
 
 func UpdateMessageProcessedStatus(spannerClient *spanner.Client, id string) error {
 	if id == "" {
-		glog.Info("[ERROR]: Received invalid message ID")
+		glog.Info("[Acknowledge]: Received invalid message ID")
 		return nil
 	}
 
@@ -41,10 +41,10 @@ func UpdateMessageProcessedStatus(spannerClient *spanner.Client, id string) erro
 		spanner.Update("Messages", []string{"id", "processed", "updatedAt"}, []interface{}{id, true, spanner.CommitTimestamp}),
 	})
 	if err != nil {
-		glog.Infof("[ERROR]: Failed to update message processed status in Spanner: %v", err)
+		glog.Infof("[Acknowledge]: Failed to update message processed status in Spanner: %v", err)
 		return err
 	}
 
-	glog.Infof("[STORAGE]: Updated message processed status in Spanner for ID: %s, Processed: %v", id, true)
+	glog.Infof("[Acknowledge]: Updated message processed status in Spanner for ID: %s, Processed: %v", id, true)
 	return nil
 }
