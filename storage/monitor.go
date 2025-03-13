3c1535ceb5d6dc72e81aae1016d2d3c267440813
tituscarl <titusranario@gmail.com>
2025-03-13 08:32:36 +0800

    separate monitor

---
+++ storage/monitor.go
@@ -0,0 +1,61 @@
+package storage
+
+import (
+	"context"
+	"encoding/json"
+	"time"
+
+	"github.com/golang/glog"
+)
+
+func MonitorActivity(ctx context.Context) {
+	ticker := time.NewTicker(1 * time.Minute)
+	defer ticker.Stop()
+
+	do := func() {
+		var topicMsgCounts = make(map[string]int)
+		var topicSubDetails = make(map[string]int)
+
+		topicMsgMu.RLock()
+		for topic, msgs := range TopicMessages {
+			count := 0
+			for _, msg := range msgs.GetAll() {
+				if !msg.IsFinalDeleted() {
+					count++
+				}
+			}
+			topicMsgCounts[topic] = count
+		}
+		topicMsgMu.RUnlock()
+
+		topicSubsMu.RLock()
+		for topic, subs := range topicSubs {
+			topicSubDetails[topic] = len(subs)
+		}
+		topicSubsMu.RUnlock()
+
+		if len(topicSubDetails) == 0 {
+			glog.Info("[Storage Monitor] No topic-subscription data available")
+		} else {
+			b, _ := json.Marshal(topicSubDetails)
+			glog.Infof("[Storage Monitor] Topic-Subscription data: %s", string(b))
+		}
+
+		if len(topicMsgCounts) == 0 {
+			glog.Info("[Storage Monitor] No Messages available")
+		} else {
+			b, _ := json.Marshal(topicMsgCounts)
+			glog.Infof("[Storage Monitor] Topic-Messages data: %s", string(b))
+		}
+	}
+
+	do()
+	for {
+		select {
+		case <-ctx.Done():
+			return
+		case <-ticker.C:
+			do()
+		}
+	}
+}
