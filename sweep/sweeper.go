c74a113f9c9cd0a0966e79ae32c3fb9a782685b1
tituscarl <titusranario@gmail.com>
2025-04-01 10:08:28 +0800

    rename

--- sweep/sweeper.go
+++ sweep/sweeper.go
@@ -30,6 +30,7 @@ func RunCheckForExpired(ctx context.Context) {
 						if t.Age.IsZero() {
 							continue
 						}
+						t.Mu.Lock()
 						switch {
 						case time.Since(t.Age).Seconds() >= 30 && atomic.LoadInt32(&t.AutoExtend) == 0:
 							t.Unlock()
@@ -37,6 +38,7 @@ func RunCheckForExpired(ctx context.Context) {
 						case time.Since(t.Age).Seconds() >= 30 && atomic.LoadInt32(&t.AutoExtend) == 1:
 							t.RenewAge()
 						}
+						t.Mu.Unlock()
 					}
 					if count == len(v1.Subscriptions) {
 						v1.MarkAsFinalDeleted()
