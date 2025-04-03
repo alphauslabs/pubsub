54c1598d382bb7a3573386e1ce9bcd883d98909c
tituscarl <titusranario@gmail.com>
2025-04-03 13:13:28 +0800

    try remove monitoring

--- main.go
+++ main.go
@@ -19,7 +19,6 @@ import (
 	"github.com/alphauslabs/pubsub/app"
 	"github.com/alphauslabs/pubsub/handlers"
 	"github.com/alphauslabs/pubsub/leader"
-	"github.com/alphauslabs/pubsub/storage"
 	"github.com/alphauslabs/pubsub/sweep"
 	"github.com/alphauslabs/pubsub/utils"
 	"github.com/flowerinthenight/hedge"
@@ -143,7 +142,7 @@ func main() {
 		}
 	}()
 
-	go storage.MonitorActivity(ctx)
+	// go storage.MonitorActivity(ctx)
 	// Start our sweeper goroutine to check if message is expired, if so, then it unlocks it.
 	go sweep.RunCheckForExpired(ctx)
 	// Start our sweeper goroutine to check if message is deleted, if so, then it deletes it.
